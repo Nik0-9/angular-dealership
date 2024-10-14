b899bf4898953eab7a371f4523c2b61e4e8fe638
Nik0-9 <filannino.nicola9@gmail.com>
2024-10-14 16:39:57 +0200

    add error message in login page

--- src/app/pages/Login/login.component.ts
+++ src/app/pages/Login/login.component.ts
@@ -1,36 +1,42 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Ruolo, DB, Utente } from '../../types/db.type';
 import { CommonModule } from '@angular/common';
-
-
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LogInComponent {
-
   loginForm: FormGroup;
+  formError: string | null = null;
 
   constructor(private form: FormBuilder, private router: Router) {
     this.loginForm = this.form.group({
       role: ['', Validators.required],
       user: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8)]]
-    })
-   }
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
 
-   validateUser(username: string, password: string): Utente | null {
-    const user = DB.utenti.find((utente) => utente.username === username && utente.password === password);
+  validateUser(username: string, password: string): Utente | null {
+    const user = DB.utenti.find(
+      (utente) => utente.username === username && utente.password === password
+    );
     return user || null;
   }
 
-   onSubmit() {
+  onSubmit() {
+    this.formError = null;
     const username = this.loginForm.get('user')?.value;
     const password = this.loginForm.get('password')?.value;
     const role = this.loginForm.get('role')?.value;
@@ -38,20 +44,20 @@ export class LogInComponent {
     if (this.loginForm.valid) {
       const validUser = this.validateUser(username, password);
 
-    if (validUser) {
-      // Verifica il ruolo dell'utente
-      if (validUser.ruolo === Ruolo.ADMIN && role === Ruolo.ADMIN) {
-        this.router.navigate(['/showcase']); // Naviga alla vetrina per admin
-      } else if (validUser.ruolo === Ruolo.USER && role === Ruolo.USER) {
-        this.router.navigate(['']); // Naviga alla home per user
+      if (validUser) {
+        // Verifica il ruolo dell'utente
+        if (validUser.ruolo === Ruolo.ADMIN && role === Ruolo.ADMIN) {
+          this.router.navigate(['/showcase']);
+        } else if (validUser.ruolo === Ruolo.USER && role === Ruolo.USER) {
+          this.router.navigate(['/sold']);
+        } else {
+          this.formError = 'Ruolo non corretto per questo utente';
+        }
       } else {
-        console.log('Ruolo non corretto per questo utente');
+        this.formError = 'Username o password non validi';
       }
     } else {
-      console.log('Username o password non validi');
+      console.log('Form non valido');
     }
-  } else {
-    console.log('Form non valido');
   }
-}
 }
