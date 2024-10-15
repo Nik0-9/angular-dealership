bdbb75235731ca65dfa3ab2d2ec918184558e03a
Nik0-9 <filannino.nicola9@gmail.com>
2024-10-16 01:29:30 +0200

    Use vehicle-showcase component to show vehicle for sale and also vehicle sold

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { LogInComponent } from './pages/Login/login.component';
 import { VehicleShowcaseComponent } from './pages/vehicle-showcase/vehicle-showcase.component';
-import { VehicleSoldComponent } from './pages/vehicle-sold/vehicle-sold.component';
 import { StatsPageComponent } from './pages/stats-page/stats-page.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LogInComponent},
     {path: 'showcase', component: VehicleShowcaseComponent},
-    {path: 'sold', component: VehicleSoldComponent},
+    {path: 'sold', component: VehicleShowcaseComponent},
     {path: 'stats', component:StatsPageComponent}
     
 ];
--- src/app/pages/vehicle-showcase/vehicle-showcase.component.ts
+++ src/app/pages/vehicle-showcase/vehicle-showcase.component.ts
@@ -3,32 +3,49 @@ import { CommonModule } from '@angular/common';
 import { DB, Veicolo, Stato, TipoVeicolo } from '../../types/db.type'; 
 import { VehicleCardComponent } from '../../component/vehicle-card/vehicle-card.component';
 import { VehicleFilterComponent } from '../../component/vehicle-filter/vehicle-filter.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-showcase',
   standalone: true,
-  imports: [CommonModule, VehicleCardComponent, VehicleFilterComponent ],
+  imports: [CommonModule, VehicleCardComponent, VehicleFilterComponent],
   templateUrl: './vehicle-showcase.component.html',
-  styleUrl: './vehicle-showcase.component.scss',
+  styleUrls: ['./vehicle-showcase.component.scss'],
 })
 export class VehicleShowcaseComponent {
-  vehiclesForSale: Veicolo[] = [];
+  vehicles: Veicolo[] = [];
   filteredVehicles: Veicolo[] = [];
-  TipoVeicolo = TipoVeicolo
-  constructor() {
-    this.vehiclesForSale = DB.veicoli.filter(
-      (veicolo) => veicolo.stato === Stato.VENDESI
-    );
-    this.filteredVehicles = [...this.vehiclesForSale];
+  pageTitle: string = '';
+  TipoVeicolo = TipoVeicolo;
+  selectedType: TipoVeicolo | null = null;
+
+  constructor(private route: ActivatedRoute) {
+    // Controlla la URL per determinare lo stato dei veicoli da mostrare e il titolo
+    this.route.url.subscribe(url => {
+      if (url.some(segment => segment.path === 'sold')) {
+        // Mostra veicoli venduti e aggiorna il titolo
+        this.pageTitle = 'Veicoli Venduti';
+        this.vehicles = DB.veicoli.filter(veicolo => veicolo.stato === Stato.VENDUTO);
+      } else {
+        // Mostra veicoli in vendita e aggiorna il titolo
+        this.pageTitle = 'Veicoli in Vendita';
+        this.vehicles = DB.veicoli.filter(veicolo => veicolo.stato === Stato.VENDESI);
+      }
+      this.filteredVehicles = this.vehicles;
+    });
   }
 
-  applyFilter(type:TipoVeicolo){
-    console.log(`filtraggio per tipo: ${type}`);
-    this.filteredVehicles = this.vehiclesForSale.filter((veicolo) => veicolo.tipo === type);
+  applyFilter(type: TipoVeicolo) {
+    if (this.selectedType === type) {
+      this.selectedType = null;  // Deseleziona il filtro
+      this.filteredVehicles = this.vehicles;  // Mostra tutti i veicoli
+    } else {
+      this.selectedType = type;  // Applica il filtro selezionato
+      this.filteredVehicles = this.vehicles.filter(veicolo => veicolo.tipo === type);
+    }
   }
 
-  resetFilter(){
-    this.filteredVehicles = [...this.vehiclesForSale];
+  isFilterActive(type: TipoVeicolo): boolean {
+    return this.filteredVehicles.every(veicolo => veicolo.tipo === type);
   }
 }
