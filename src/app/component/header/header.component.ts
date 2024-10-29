07698693975a57ad0c99e4a15052d9117ac835b0
Nik0-9 <filannino.nicola9@gmail.com>
2024-10-29 11:26:47 +0100

    fix minor

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
@@ -11,21 +11,9 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  isSmallScreen: boolean = false;
+ 
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {
-    this.checkScreenSize();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.checkScreenSize();
-  }
-
-  private checkScreenSize() {
-    this.isSmallScreen = window.innerWidth < 768;
-  }
 
   get isUserLoggedIn() {
     return this.authService.isLoggedIn;
